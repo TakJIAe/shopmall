1b800b2fcf6a5fc79b1da1994b67f1d2c1304ed1
TakJIAe <hiworldd@naver.com>
2021-06-15 23:55:27 +0900

    need update BoardController

--- shopmall/src/main/java/kr/jeje/shopmall/controller/BoardController.java
+++ shopmall/src/main/java/kr/jeje/shopmall/controller/BoardController.java
@@ -1,11 +1,15 @@
 package kr.jeje.shopmall.controller;
 
-import kr.jeje.shopmall.domain.entity.Board;
+import kr.jeje.shopmall.domain.entity.File;
 import kr.jeje.shopmall.dto.BoardDto;
+import kr.jeje.shopmall.dto.FileDto;
 import kr.jeje.shopmall.service.BoardService;
+import kr.jeje.shopmall.service.FileService;
+import kr.jeje.shopmall.util.MD5Generator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -13,9 +17,11 @@ import java.util.List;
 public class BoardController {
 
     private BoardService boardService;
+    private FileService fileService;
 
-    public BoardController(BoardService boardService){
+    public BoardController(BoardService boardService, FileService fileService){
         this.boardService = boardService;
+        this.fileService  = fileService;
     }
 
     @GetMapping("/")
@@ -31,8 +37,36 @@ public class BoardController {
     }
 
     @PostMapping("/post")
-    public String write(BoardDto boardDto){
-        boardService.savePost(boardDto);
+    public String write(@RequestParam("file") MultipartFile files, BoardDto boardDto) {
+        try {
+            String origFilename = files.getOriginalFilename();
+            String filename = new MD5Generator(origFilename).toString();
+            /* 실행되는 위치의 'files' 폴더에 파일이 저장됩니다. */
+            String savePath = System.getProperty("user.dir") + "\\files";
+
+            /* 파일이 저장되는 폴더가 없으면 폴더를 생성합니다. */
+            if (!new File(savePath).exists()) {
+                try{
+                    new File(savePath).mkdir();
+                }
+                catch(Exception e){
+                    e.getStackTrace();
+                }
+            }
+            String filePath = savePath + "\\" + filename;
+            files.transferTo(new File(filePath));
+
+            FileDto fileDto = new FileDto();
+            fileDto.setOrigFilename(origFilename);
+            fileDto.setFilename(filename);
+            fileDto.setFilePath(filePath);
+
+            Long fileId = fileService.saveFile(fileDto);
+            boardDto.setFileId(fileId);
+            boardService.savePost(boardDto);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
         return "redirect:/";
     }
 
